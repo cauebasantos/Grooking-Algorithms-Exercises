5e7c48a67ae4fcf63f2d8be1621ad46d9f1e3bea
Caue Santos <cauebasantos@gmail.com>
2018-11-20 17:23:45 -0300

    Fix dac_binary_search.py, now it's working

--- Chapter_4/dac_binary_search.py
+++ Chapter_4/dac_binary_search.py
@@ -3,8 +3,13 @@ def dac_binary_search(arr, item):
     if item == arr[mid]:
         return mid
     elif item < arr[mid]:
-        dac_binary_search(arr[:mid], item)
+        return dac_binary_search(arr[:mid], item)
     else:
-        dac_binary_search(arr[mid+1:], item)
+        return mid + 1 + dac_binary_search(arr[mid+1:], item)
 
-print(dac_binary_search([1, 3, 5, 14, 28, 99], 14))
+print(dac_binary_search([1, 3, 5, 14, 28, 99],  1))
+print(dac_binary_search([1, 3, 5, 14, 28, 99],  3))
+print(dac_binary_search([1, 3, 5, 14, 28, 99],  5)) 
+print(dac_binary_search([1, 3, 5, 14, 28, 99],  14))
+print(dac_binary_search([1, 3, 5, 14, 28, 99],  28))
+print(dac_binary_search([1, 3, 5, 14, 28, 99],  99)) 
